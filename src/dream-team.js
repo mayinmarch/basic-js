4e7610d66a7b524b5cb2962040186499c81a2747
mayinmarch <marfa.astafieva@gmail.com>
2020-03-17 01:14:52 +0300

    feat: implement dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){
+    return false;
+  }
+  let abbr = [];
+  members.forEach(element => {
+    if(typeof element === 'string'){
+      element = element.trim().toUpperCase();
+      abbr.push(element[0]);
+    }
+  })
+  let result = '';
+  abbr.sort();
+  abbr.forEach(element => result += element);
+  return result;
 };
