16ade8fd81e23d772ce96e5a4cac0fb6b603c06a
mayinmarch <marfa.astafieva@gmail.com>
2020-03-21 01:19:12 +0300

    feat: implement hanoi towers

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const K = 0.693 / HALF_LIFE_PERIOD; 
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  let activity = Number.parseFloat(sampleActivity);
+  if (isNaN(activity)) {
+    return false;
+  }
+  if (activity <= 0) {
+    return false;
+  }
+  if (activity > 15) {
+    return false;
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / K);
+    
 };
