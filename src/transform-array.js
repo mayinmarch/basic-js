e401a829d2800df11b7b1aa4a3409db4f23f38fc
mayinmarch <marfa.astafieva@gmail.com>
2020-03-21 21:35:20 +0300

    feat: implement  transformed-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)){
+        throw Error('is not array');
+    }
+
+    let resultArr = [];
+    for(let indx = 0; indx < arr.length; indx++){
+        switch(arr[indx]){
+            case '--discard-next':
+                if(indx !== arr.length - 1){
+                    indx = indx + 1;
+                }
+                break;
+            case '--discard-prev':
+                if(indx !== 0){
+                    resultArr.pop();
+                }
+                break;
+            case '--double-next':
+                if(indx !== arr.length - 1){
+                    resultArr.push(arr[indx+1]);
+                }
+                break;
+            case '--double-prev':
+                if(indx !== 0){
+                    resultArr.push(arr[indx-1]);
+                }
+                break;
+            default:
+                resultArr.push(arr[indx]);
+        }
+    }
+    return resultArr;
 };
+
