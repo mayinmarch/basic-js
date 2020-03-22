17a70d32db151ede89e29bea0370cc91fbe662e1
mayinmarch <marfa.astafieva@gmail.com>
2020-03-22 23:49:25 +0300

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,86 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+
+function letterPosition(letter){
+    return ALPHABET.indexOf(letter.toUpperCase());
+}
+
+function positionToLetter(position){
+    return ALPHABET[position];
+}
+
+function isLetter(character){
+    if(letterPosition(character) === -1){
+        return false;
+    } else {
+        return true;
+    }
+}
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect = true){
+        this.isDirect = isDirect;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(message === undefined){
+            throw Error('message undefined');
+        }
+        if(key === undefined){
+            throw Error ('key undefined');
+        }
+
+        let result = '';
+        let keyIndex = 0;
+        message.split('').forEach((e) => {
+            if(isLetter(e)){
+                let x = letterPosition(e);
+                let y = letterPosition(key[keyIndex]);
+                result += positionToLetter((x + y) % 26);
+                if(keyIndex === key.length - 1){
+                    keyIndex = 0;
+                } else {
+                    keyIndex += 1;
+                }  
+            } else {
+                result += e; 
+            }
+        })
+        if(!this.isDirect){
+            return result.split('').reverse().join('');
+        } else {
+            return result;
+        }
+    }
+
+    decrypt(message, key) {
+        if(message === undefined){
+            throw Error('message undefined');
+        }
+        if(key === undefined){
+            throw Error ('key undefined');
+        }
+        
+        let result = '';
+        let keyIndex = 0;
+        message.split('').forEach((e) => {
+            if(isLetter(e)){
+                let x = letterPosition(e);
+                let y = letterPosition(key[keyIndex]);
+                result += positionToLetter((26 + x - y) % 26);
+                if(keyIndex === key.length - 1){
+                    keyIndex = 0;
+                } else {
+                    keyIndex += 1;
+                }  
+            } else {
+                result += e; 
+            }
+        })
+        if(!this.isDirect){
+            return result.split('').reverse().join('');
+        } else {
+            return result;
+        }
     }
 }
 
